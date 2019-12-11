4b98330873af45426745045ed3bbe0e69840ee55
wywwwwei <wyw1191448318@hotmail.com>
2019-12-11 15:58:13 +0800

    commit crawler

---
+++ Crawler/crawler.go
@@ -0,0 +1,412 @@
+package crawler
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+)
+
+const (
+	dbPath          = "./data/data.db"
+	dbMode          = 0600
+	PeopleBucket    = "PeopleBucket"
+	PlanetsBucket   = "PlanetsBucket"
+	StarshipsBucket = "StarshipsBucket"
+	FilmsBucket     = "FilmsBucket"
+	SpeciesBucket   = "SpeciesBucket"
+	VehiclesBucket  = "VehiclesBucket"
+)
+
+type Crawler struct {
+	client *http.Client
+}
+
+// Get a crawler using the default client
+func GetCrawlerSync() *Crawler {
+	return &Crawler{
+		client: http.DefaultClient,
+	}
+}
+
+// Get a crawler using a custom client
+func GetCrawlerAsync(c *http.Client) *Crawler {
+	return &Crawler{
+		client: c,
+	}
+}
+
+// Use coroutines to crawl different information to improve performance
+func ScrapAndSave() {
+	// Open the db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open(dbPath, dbMode, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	wg := sync.WaitGroup{}
+	wg.Add(6)
+
+	go func() {
+		people, err := GetCrawlerAsync(&http.Client{}).getPeopleInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range people {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, PeopleBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		planets, err := GetCrawlerAsync(&http.Client{}).getPlanetsInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range planets {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, PlanetsBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		starships, err := GetCrawlerAsync(&http.Client{}).getStarshipsInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range starships {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, StarshipsBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		films, err := GetCrawlerAsync(&http.Client{}).getFilmsInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range films {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, FilmsBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		species, err := GetCrawlerAsync(&http.Client{}).getSpeciesInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range species {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, SpeciesBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		vehicles, err := GetCrawlerAsync(&http.Client{}).getVehiclesInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, val := range vehicles {
+			jsonData, err := json.Marshal(val)
+			if err != nil {
+				log.Print(err)
+			}
+
+			if err := writeToDB(db, VehiclesBucket, val.ID, jsonData); err != nil {
+				log.Print(err)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
+}
+
+// Write key-[] byte key-value pairs to a bucket named bucket
+func writeToDB(db *bolt.DB, bucket, key string, data []byte) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(bucket))
+		if err != nil {
+			log.Print(err)
+			return err
+		}
+		return b.Put([]byte(key), data)
+	})
+}
+
+// Request method used by the crawler, while parsing the returned json data into v
+func (c *Crawler) requestAndSave(u string, v interface{}) error {
+	fmt.Println(u)
+	res, err := c.client.Get(u)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := res.Body.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return json.NewDecoder(res.Body).Decode(v)
+}
+
+// Crawl all people info
+func (c *Crawler) getPeopleInfo() ([]People, error) {
+	peopleInfo := struct {
+		Next   *string  `json:"next"`
+		Person []People `json:"results"`
+	}{}
+	requestURL, err := composeUrl("people")
+	if err != nil {
+		return nil, err
+	}
+
+	var people []People
+	for {
+		err = c.requestAndSave(requestURL, &peopleInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		people = append(people, peopleInfo.Person...)
+
+		if peopleInfo.Next == nil {
+			break
+		}
+		requestURL = *peopleInfo.Next
+	}
+
+	for key := range people {
+		people[key].ID = strconv.Itoa(key + 1)
+		replaceSingleOldString(&people[key].HomeWorld)
+		replaceOldString(people[key].Starships)
+		replaceOldString(people[key].Vehicles)
+		replaceOldString(people[key].Films)
+		replaceOldString(people[key].Species)
+	}
+
+	fmt.Println(people)
+	return people, nil
+}
+
+// Crawl all planets info
+func (c *Crawler) getPlanetsInfo() ([]Planets, error) {
+	planetsInfo := struct {
+		Next   *string   `json:"next"`
+		Planet []Planets `json:"results"`
+	}{}
+	requestURL, err := composeUrl("planets")
+	if err != nil {
+		return nil, err
+	}
+
+	var planets []Planets
+	for {
+		err = c.requestAndSave(requestURL, &planetsInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		planets = append(planets, planetsInfo.Planet...)
+		if planetsInfo.Next == nil {
+			break
+		}
+		requestURL = *planetsInfo.Next
+	}
+
+	for key := range planets {
+		planets[key].ID = strconv.Itoa(key + 1)
+		replaceOldString(planets[key].Films)
+		replaceOldString(planets[key].Residents)
+	}
+
+	fmt.Println(planets)
+	return planets, nil
+}
+
+// Crawl all starships info
+func (c *Crawler) getStarshipsInfo() ([]Starships, error) {
+	starshipsInfo := struct {
+		Next     *string     `json:"next"`
+		Starship []Starships `json:"results"`
+	}{}
+	requestURL, err := composeUrl("starships")
+	if err != nil {
+		return nil, err
+	}
+
+	var starships []Starships
+	for {
+		err = c.requestAndSave(requestURL, &starshipsInfo)
+		if err != nil {
+			return nil, err
+		}
+
+		starships = append(starships, starshipsInfo.Starship...)
+		if starshipsInfo.Next == nil {
+			break
+		}
+		requestURL = *starshipsInfo.Next
+	}
+
+	for key := range starships {
+		starships[key].ID = strconv.Itoa(key + 1)
+		replaceOldString(starships[key].Films)
+		replaceOldString(starships[key].Pilots)
+	}
+
+	fmt.Println(starships)
+	return starships, nil
+}
+
+// Crawl all films info
+func (c *Crawler) getFilmsInfo() ([]Films, error) {
+	filmsInfo := struct {
+		Next *string `json:"next"`
+		Film []Films `json:"results"`
+	}{}
+	requestURL, err := composeUrl("films")
+	if err != nil {
+		return nil, err
+	}
+
+	var films []Films
+	for {
+		err = c.requestAndSave(requestURL, &filmsInfo)
+		if err != nil {
+			return nil, err
+		}
+		films = append(films, filmsInfo.Film...)
+
+		if filmsInfo.Next == nil {
+			break
+		}
+		requestURL = *filmsInfo.Next
+	}
+
+	for key := range films {
+		films[key].ID = strconv.Itoa(key + 1)
+		replaceOldString(films[key].Species)
+		replaceOldString(films[key].Vehicles)
+		replaceOldString(films[key].Starships)
+		replaceOldString(films[key].Planets)
+		replaceOldString(films[key].Character)
+	}
+
+	fmt.Println(films)
+	return films, nil
+}
+
+// Crawl all species info
+func (c *Crawler) getSpeciesInfo() ([]Species, error) {
+	speciesInfo := struct {
+		Next   *string   `json:"next"`
+		Specie []Species `json:"results"`
+	}{}
+	requestURL, err := composeUrl("species")
+	if err != nil {
+		return nil, err
+	}
+
+	var species []Species
+	for {
+		err = c.requestAndSave(requestURL, &speciesInfo)
+		if err != nil {
+			return nil, err
+		}
+		species = append(species, speciesInfo.Specie...)
+
+		if speciesInfo.Next == nil {
+			break
+		}
+		requestURL = *speciesInfo.Next
+	}
+
+	for key := range species {
+		species[key].ID = strconv.Itoa(key + 1)
+		replaceOldString(species[key].Films)
+		replaceOldString(species[key].People)
+	}
+
+	fmt.Println(species)
+	return species, nil
+}
+
+// Crawl all vehicles info
+func (c *Crawler) getVehiclesInfo() ([]Vehicles, error) {
+	vehiclesInfo := struct {
+		Next    *string    `json:"next"`
+		Vehicle []Vehicles `json:"results"`
+	}{}
+	requestURL, err := composeUrl("vehicles")
+	if err != nil {
+		return nil, err
+	}
+
+	var vehicles []Vehicles
+	for {
+		err = c.requestAndSave(requestURL, &vehiclesInfo)
+		if err != nil {
+			return nil, err
+		}
+		vehicles = append(vehicles, vehiclesInfo.Vehicle...)
+
+		if vehiclesInfo.Next == nil {
+			break
+		}
+		requestURL = *vehiclesInfo.Next
+	}
+
+	for key := range vehicles {
+		vehicles[key].ID = strconv.Itoa(key + 1)
+		replaceOldString(vehicles[key].Films)
+		replaceOldString(vehicles[key].Pilots)
+	}
+
+	fmt.Println(vehicles)
+	return vehicles, nil
+}
---
+++ Crawler/utils.go
@@ -0,0 +1,35 @@
+package crawler
+
+import (
+	"net/url"
+	"strings"
+)
+
+const (
+	oldString = "https://swapi.co"
+	newString = "http://localhost:8080/query"
+)
+
+// Combine api relative path and server address into a complete url
+func composeUrl(api string) (string, error) {
+	hostURL := url.URL{
+		Scheme:   "https",
+		Host:     "swapi.co",
+		Path:     "api/" + api,
+		RawQuery: "format=json",
+	}
+
+	return hostURL.String(), nil
+}
+
+// Replace the crawled server url with the URL of our server for string
+func replaceSingleOldString(input *string) {
+	*input = strings.Replace(*input, oldString, newString, 1)
+}
+
+// Replace the crawled server url with the URL of our server for []string
+func replaceOldString(input []string) {
+	for key := range input {
+		input[key] = strings.Replace(input[key], oldString, newString, 1)
+	}
+}
